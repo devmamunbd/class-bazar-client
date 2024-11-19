ec1c4a62fbc96064662d899c3bd14dde5c65058d
devmamunbd <itsmemamunbd@gmail.com>
2024-11-19 13:38:22 +0600

    axios interceptors setup

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,47 @@
 
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 
 
 const useAxiosSecure = () => {
-  return (
-    <div>useAxiosSecure</div>
-  )
+    const {LogOut} = useAuth();
+    const navigate = useNavigate()
+
+
+    const axiosSecure = axios.create({
+        baseURL: 'http://localhost:7000',
+    });
+
+    useEffect(()=> {
+        // Add a request interceptor
+
+       const requestInterceptor = axiosSecure.interceptors.request.use((config)=>{
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config
+        });
+
+        // Add a response interceptor
+       const responseInterceptor = axiosSecure.interceptors.response.use((response)=> response, async(error)=> {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            await LogOut();
+            navigate('/login')
+            throw error;
+        }
+        throw error;
+       });
+       return ()=> {
+        axiosSecure.interceptors.request.eject(requestInterceptor);
+        axiosSecure.interceptors.response.eject(responseInterceptor);
+       }
+
+    },[LogOut, axiosSecure, navigate])
+    return axiosSecure;
 }
 
 export default useAxiosSecure
--- src/utilities/providers/AuthProvider.jsx
+++ src/utilities/providers/AuthProvider.jsx
@@ -1,9 +1,11 @@
-import { createContext, useState } from "react"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react"
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { auth } from './../../../firebase.init';
+import axios from "axios";
 
 
 export const AuthContext = createContext()
+const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = ({children}) => {
 
@@ -37,8 +39,68 @@ const AuthProvider = ({children}) => {
     }
 
 
+    // Log Out
+    const LogOut = async()=> {
+        try {
+            return await signOut(auth)
+        } catch (error) {
+            setError(error.code)
+            throw error
+        }
+    }
 
-    const ContextValue = {user, }
+
+    // Update User Profile
+    const updateUserProfile =async(name, photo)=> {
+        try {
+            await updateProfile(auth.currentUser, {
+                displayName: name,
+                photoURL: photo,
+            })
+            setUser(auth.currentUser)
+        } catch (error) {
+            setError(error.code)
+            throw error
+        }
+    }
+
+
+    // google Login
+    const googleLogin =async()=> {
+        try {
+            setLoading(true)
+            return await signInWithPopup(auth, googleProvider)
+        } catch (error) {
+            setError(error.code)
+            throw error
+        }
+    }
+
+
+    // Observer For Users
+    useEffect(()=> {
+        const unSubscribe = onAuthStateChanged((user) => {
+            setUser(user)
+
+            if (user) {
+                axios.post("http://localhost:7000/api-set-token", {email: user.email, name: user.displayName})
+                .then((data) => {
+                    if (data.data.token) {
+                        localStorage.setItem('token', data.data.token)
+                        setLoading(false)
+                    }
+                })
+            } else {
+                localStorage.removeItem('token')
+                setLoading(false)
+            }
+        });
+        return () => {
+            unSubscribe()
+        }
+    },[])
+
+    const ContextValue = {user, singUp, singIn, updateUserProfile, googleLogin, error, setError, LogOut}
 
   return (
     <AuthContext.Provider value={ContextValue}>
